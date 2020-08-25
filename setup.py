0ad306e63fd6a1ad2e207f9197ba1011f8791ef4
mosheber <screedo1997@gmail.com>
2020-08-25 21:34:31 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="scale_fpn", # Replace with your own username
+    name="group_stratification", # Replace with your own username
     version="0.0.1",
     author="mosheber",
     author_email="screedo1997@gmail.com",
     description="a scalable fpn architecture using pytorch",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mosheber/scalable_feature_pyramid_network.git",
+    url="https://github.com/mosheber/group_stratification.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
